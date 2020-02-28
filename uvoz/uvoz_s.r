9430cd098161e4a94d49c53533d28b8ac6dfc37a
Sabrina Calcina <sabrina.calcina@student.fmf.uni-lj.si>
2020-02-28 22:49:20 +0100

    28.2.2020

--- uvoz/uvoz_s.r
+++ uvoz/uvoz_s.r
@@ -8,25 +8,18 @@ brezposelnost.regije <- read.csv2(file="podatki/brezposelnost_po_statisticnih_re
 graf_regije <- brezposelnost.regije[,-6]
 graf_regije <- graf_regije[,-4]
 graf_regije <- graf_regije[,-3]
-#graf_regije <- transform(graf_regije, stopnja_brezposelnosti=(stopnja_brezposelnosti*1000))
+  #graf_regije <- transform(graf_regije, stopnja_brezposelnosti=(stopnja_brezposelnosti*1000))
 
-#2. tabela-brezposelnost glede na stopnjo dosežene izobrazbe (vzela spodaj boljšo tabelo, pustim za zdaj če bom še rabila)
-brezposelnost.izobrazba <- read.csv2(file="podatki/brezposelnost_po_stopnji_izobrazbe.csv", header=TRUE, skip = 1, 
-                                     col.names = c( "Kohezijska_regija", "Spol", "leto",  "brez_izobrazbe", 
-                                                   "osnovnosolska_izobrazba",
-                                                   "nizja_ali_srednja",
-                                                   "srednja_strokovna",
-                                                   "visjesolska_visokosolska"))
-brezposelnost.izobrazba <- brezposelnost.izobrazba[,-1]
-
-brezposelnost.izobrazba <- transform(brezposelnost.izobrazba, brez_izobrazbe = (brez_izobrazbe * 1000))
-brezposelnost.izobrazba <- transform(brezposelnost.izobrazba, osnovnosolska_izobrazba = (osnovnosolska_izobrazba * 1000))
-brezposelnost.izobrazba <- transform(brezposelnost.izobrazba, nizja_ali_srednja = (nizja_ali_srednja * 1000))
-brezposelnost.izobrazba <- transform(brezposelnost.izobrazba, srednja_strokovna = (srednja_strokovna * 1000))
-brezposelnost.izobrazba <- transform(brezposelnost.izobrazba, visjesolska_visokosolska = (visjesolska_visokosolska * 1000))
+  #tabela za zemljevid 2010
+brezposelnost.2010 <-filter(graf_regije, leto == '2010')
 
 
-#4.tabela-brezposelnost po trajanju iskanja dela za celotno Slovenijo(ok za zemljevid)
+  #tabela za zemljevid 2018
+brezposelnost.2018 <- filter(graf_regije, leto == '2018')
+
+
+
+#2.tabela-brezposelnost po trajanju iskanja dela za celotno Slovenijo(ok za zemljevid)
 trajanje <- read.csv2(file="podatki/trajanje.csv", header=TRUE, skip = 2, na=c('N'),  
                                    col.names = c("nekaj","čas", "spol", "leto",  "skip", "brezposelnost"))
 
@@ -35,7 +28,7 @@ trajanje <- trajanje[,-3]
 trajanje <- trajanje[,-1]
 
 
-#6.tabela-brezposelnost v Sloveniji
+#3.tabela-brezposelnost v Sloveniji
 slovenija <- read.csv2(file="podatki/slovenija.csv", header=TRUE, skip = 1,  
                        col.names = c("kohezj", "skupine", "spol", "leto", "brezposelnost"))
 
@@ -44,15 +37,10 @@ slovenija <- slovenija[,-2]
 slovenija <- slovenija[,-1]
 #slovenija <- transform(slovenija, brezposelnost = (brezposelnost * 1000))
 
-#tabela za zemljevid 2010
-brezposelnost.2010 <-filter(graf_regije, leto == '2010')
 
 
-#tabela za zemljevid
-brezposelnost.2018 <- filter(graf_regije, leto == '2018')
 
-
-#7.tabela-brezposelnost glede na stopnjo dosežene izobrazbe
+#4.tabela-brezposelnost glede na stopnjo dosežene izobrazbe
 izobrazba <- read.csv2(file="podatki/izobrazba.csv", header=TRUE, skip = 2, 
                                      col.names = c("leto", "Kohezijska_regija", "Spol", "izobrazbe", 
                                                    "meritve", "brezposelnost"))
@@ -63,17 +51,18 @@ izobrazba <- izobrazba[, -2]
 #izobrazba <- transform(izobrazba, brezposelnost = (brezposelnost*1000))
 
 
-#8.tabela-kriminaliteta v Sloveniji glede na statistične regije
+#5.tabela-kriminaliteta v Sloveniji glede na statistične regije
 kriminaliteta <- read.csv2(file="podatki/kriminaliteta.csv", header=TRUE, skip = 2, 
-                       col.names = c("regija", "nekaj", "leto", "obsojeni"))        
+                       col.names = c("regija", "nekaj", "leto", "obsojeni"), dec = '.')        
 kriminaliteta <- kriminaliteta[,-2]
 
 
 
-#9.tabela za Shiny
-
+#tabela za Shiny
 povprecje <- left_join(kriminaliteta, graf_regije, by=c('regija','leto'))
 colnames(povprecje) <- c('regija', 'leto', 'obsojeni',  'stopnja_brezposelnost')
+
+povprecje$stopnja_brezposelnost <- as.numeric(povprecje$stopnja_brezposelnost)
 
 
 
--- vizualizacija/vizualizacija_sabrina.r
+++ vizualizacija/vizualizacija_sabrina.r
@@ -14,13 +14,13 @@ source('uvoz/uvoz_s.r', encoding = 'UTF-8')
 
 
 #Brezposelnost v Sloveniji na splošno
-osnovni_graf <- ggplot(slovenija, aes(x=leto, y=brezposelnost), )+
+osnovni_graf <- ggplot(slovenija, aes(x=leto, y=brezposelnost, fill="5"), )+
   ggtitle('Brezposelnost glede na celotno Slovenijo v 1000')+
   geom_bar(stat = 'identity', position = 'dodge')
 
 
 
-#Graf stopnje brezposelnosti skozi leta glede na staistične regije
+#Graf stopnje brezposelnosti skozi leta glede na staistične
 vec_graf <- ggplot(data = graf_regije, mapping = aes(x=regija, y=stopnja_brezposelnosti, fill=factor(leto))) +
   labs(fill='leto') +
   ggtitle('Brezposelnost na 1000 prebivalcev po regijah') +
@@ -65,11 +65,21 @@ zemljevid.brezposelnost.2018 <- ggplot() +
 
 #Grafki brezposelnosti v Sloveniji glede na stopnjo dosežene izobrazbe
 
-graf_izobrazba <- ggplot(data = izobrazba, mapping = aes(x=leto, y=brezposelnost)) +
+graf_izobrazba <- ggplot(data = izobrazba, mapping = aes(x=leto, y=brezposelnost, col="4")) +
   ggtitle('Brezposelnost glede na stopnjo dosežene izobrazbe') +
   geom_point() +
   #facet_grid( Drzava~.) +
   facet_wrap(izobrazbe~., ncol=3) +
   theme(axis.text.x = element_text(angle = 90, size = 6))
+
+
+#Brezposelnost glede na trajanje iskanja dela
+
+graf_trajanje <- ggplot((data = trajanje), aes(x=leto, y=brezposelnost, col=cas)) + geom_point() + geom_line() +
+  scale_x_continuous('Leto', breaks = seq(2008, 2018, 1), limits = c(2008,2018)) +
+  ggtitle('Brezposelnost na 1000 prebivalcev glede na čas iskanja dela')
+
+
+
 
 
